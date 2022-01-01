19efbf2ed46ced5105c089a2d4b95702288bc28f
zhanglun <zhanhundao@126.com>
2022-01-01 15:15:16 +1300

    Train script print train data path

--- pytorch-rnn-scripts/train.py
+++ pytorch-rnn-scripts/train.py
@@ -93,6 +93,11 @@ train_data = batchify(corpus.train, args.batch_size)
 val_data = batchify(corpus.valid, eval_batch_size)
 test_data = batchify(corpus.test, eval_batch_size)
 
+#张伦 debug
+print('Train data {}'.format(train_data))
+print('Val data {}'.format(val_data))
+print('Test data {}'.format(test_data))
+
 ###############################################################################
 # Build the model
 ###############################################################################
@@ -169,36 +174,36 @@ def evaluate(data_source):
 
 def train():
     # Turn on training mode which enables dropout.
-    model.train()
-    total_loss = 0.
-    start_time = time.time()
-    hidden = model.init_hidden(args.batch_size)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
-        data, targets = get_batch(train_data, i)
-        # Starting each batch, we detach the hidden state from how it was previously produced.
-        # If we didn't, the model would try backpropagating all the way to start of the dataset.
-        hidden = repackage_hidden(hidden)
-        model.zero_grad()
-        output, hidden = model(data, hidden)
-        loss = criterion(output.view(-1, ntokens), targets)
-        loss.backward()
-
-        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
-        torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
-        for p in model.parameters():
-            p.data.add_(-lr, p.grad.data)
-
-        total_loss += loss.item()
-
-        if batch % args.log_interval == 0 and batch > 0:
-            cur_loss = total_loss / args.log_interval
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                  'loss {:5.2f} | ppl {:8.2f}'.format(
-                      epoch, batch, len(train_data) // args.bptt, lr,
-                      elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
-            total_loss = 0
-            start_time = time.time()
+    # model.train()
+    # total_loss = 0.
+    # start_time = time.time()
+    # hidden = model.init_hidden(args.batch_size)
+    # for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+    #     data, targets = get_batch(train_data, i)
+    #     # Starting each batch, we detach the hidden state from how it was previously produced.
+    #     # If we didn't, the model would try backpropagating all the way to start of the dataset.
+    #     hidden = repackage_hidden(hidden)
+    #     model.zero_grad()
+    #     output, hidden = model(data, hidden)
+    #     loss = criterion(output.view(-1, ntokens), targets)
+    #     loss.backward()
+    #
+    #     # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+    #     torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+    #     for p in model.parameters():
+    #         p.data.add_(-lr, p.grad.data)
+    #
+    #     total_loss += loss.item()
+    #
+    #     if batch % args.log_interval == 0 and batch > 0:
+    #         cur_loss = total_loss / args.log_interval
+    #         elapsed = time.time() - start_time
+    #         print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+    #               'loss {:5.2f} | ppl {:8.2f}'.format(
+    #                   epoch, batch, len(train_data) // args.bptt, lr,
+    #                   elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+    #         total_loss = 0
+    #         start_time = time.time()
 
 
 # Loop over epochs.
